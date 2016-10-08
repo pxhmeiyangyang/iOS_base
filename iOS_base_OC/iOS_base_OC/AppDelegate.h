23294e69f0a8c75bd8703bbbbfa4255af701d352
pxh <peixinhua@unisound.com>
2016-10-08 14:39:21 +0800

    更新主题：创建新的项目 更新内容：

---
+++ iOS_base_OC/iOS_base_OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_base_OC
+//
+//  Created by pxh on 2016/10/8.
+//  Copyright © 2016年 pxh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_base_OC/iOS_base_OCTests/iOS_base_OCTests.m
@@ -0,0 +1,39 @@
+//
+//  iOS_base_OCTests.m
+//  iOS_base_OCTests
+//
+//  Created by pxh on 2016/10/8.
+//  Copyright © 2016年 pxh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOS_base_OCTests : XCTestCase
+
+@end
+
+@implementation iOS_base_OCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
