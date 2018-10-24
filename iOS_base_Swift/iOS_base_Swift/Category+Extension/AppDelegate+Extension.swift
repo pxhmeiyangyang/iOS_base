6d5b2156cac590525e074c0e3d24c86744839988
pxh <peixinhua@unisound.com>
2018-10-24 18:16:17 +0800

    remove main story board

---
+++ iOS_base_Swift/iOS_base_Swift/Category+Extension/AppDelegate+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate+Extension.swift
+//  iOS_base_Swift
+//
+//  Created by pxh on 2018/10/24.
+//  Copyright © 2018 pxh. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - AppDelegate 的拓展文件
+extension AppDelegate{
+    
+}
